26ecf53a362eb714e925287e8bcb63367e047345
Max Levin <max@prodstack.dev>
2022-06-29 09:15:08 +0000

    revert

--- cmd/main.go
+++ cmd/main.go
@@ -9,13 +9,14 @@ import (
 )
 
 const (
+	SENTRY_DEV_DSN  = "https://6420be38b4544852a61df1d7ec56f442@o1295881.ingest.sentry.io/6521982"
 	SENTRY_PROD_DSN = "https://a8ac7024755f47e5b5d4ae620499c7f6@o1295881.ingest.sentry.io/6521983"
 )
 
 func main() {
 	sentryDsn := SENTRY_PROD_DSN
 	if cmd.IsDevVersion() {
-		sentryDsn = ""
+		sentryDsn = SENTRY_DEV_DSN
 	}
 
 	err := sentry.Init(sentryDsn)
