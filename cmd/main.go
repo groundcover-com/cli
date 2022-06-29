757ab5d77f185dccc8fd5d1f5bb78d0f1e5626e3
Max Levin <max@prodstack.dev>
2022-06-29 09:11:19 +0000

    all events will be reported to prod sentry project

--- cmd/main.go
+++ cmd/main.go
@@ -9,14 +9,13 @@ import (
 )
 
 const (
-	SENTRY_DEV_DSN  = "https://6420be38b4544852a61df1d7ec56f442@o1295881.ingest.sentry.io/6521982"
 	SENTRY_PROD_DSN = "https://a8ac7024755f47e5b5d4ae620499c7f6@o1295881.ingest.sentry.io/6521983"
 )
 
 func main() {
 	sentryDsn := SENTRY_PROD_DSN
 	if cmd.IsDevVersion() {
-		sentryDsn = SENTRY_DEV_DSN
+		sentryDsn = ""
 	}
 
 	err := sentry.Init(sentryDsn)
