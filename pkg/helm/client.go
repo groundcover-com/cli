fc1cdf3a5533dc693f8c98627f064678a3bbcd6d
steb-veksler <62982582+steb-veksler@users.noreply.github.com>
2022-09-19 21:23:13 +0300

    [sc-4856] override departed authentication api version (#88)

--- pkg/helm/client.go
+++ pkg/helm/client.go
@@ -1,13 +1,17 @@
 package helm
 
 import (
+	"fmt"
 	"os"
 	"path/filepath"
 
 	"github.com/containerd/containerd/log"
+	"groundcover.com/pkg/k8s"
 	"groundcover.com/pkg/utils"
 	"helm.sh/helm/v3/pkg/action"
 	"helm.sh/helm/v3/pkg/cli"
+	"k8s.io/cli-runtime/pkg/genericclioptions"
+	"k8s.io/client-go/rest"
 )
 
 type Client struct {
@@ -33,7 +37,17 @@ func NewHelmClient(namespace, kubecontext string) (*Client, error) {
 	helmClient.settings.SetNamespace(namespace)
 	helmClient.settings.KubeContext = kubecontext
 
-	if err = helmClient.cfg.Init(helmClient.settings.RESTClientGetter(), helmClient.settings.Namespace(), os.Getenv("HELM_DRIVER"), log.L.Debugf); err != nil {
+	getter, ok := helmClient.settings.RESTClientGetter().(*genericclioptions.ConfigFlags)
+	if !ok {
+		return nil, fmt.Errorf("failed to cast helm rest client getter")
+	}
+
+	getter.WrapConfigFn = func(restConfig *rest.Config) *rest.Config {
+		k8s.OverrideDepartedAuthenticationApiVersion(restConfig)
+		return restConfig
+	}
+
+	if err = helmClient.cfg.Init(getter, helmClient.settings.Namespace(), os.Getenv("HELM_DRIVER"), log.L.Debugf); err != nil {
 		return nil, err
 	}
 
